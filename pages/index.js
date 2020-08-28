bf63d8148e20deefe19da9736341e2d6a7800d87
Rizki Hutama <rizkihutama69@gmail.com>
2020-08-28 16:34:27 +0700

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,6 @@ export default function Home({ allpostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-
         <p>
           (This is a sample website - you'll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>)
